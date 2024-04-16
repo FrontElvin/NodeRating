924325a6e583613542095bec7f5a0c2be11ac527
FrontElvin <rustamove629@gmail.com>
2024-04-16 17:31:19 +0300

    modified:   controllers/main.js

--- controllers/main.js
+++ controllers/main.js
@@ -5,6 +5,7 @@ const {PATH_TO_RATING_FILE, makeRatingFile} = require("../appModules/rating");
 
 async function mainRouteController(res, publicUrl, extname) {
     const data = await getData(endpoints.games);
+    console.log(data);
     makeRatingFile(PATH_TO_RATING_FILE, data);
     res.statusCode = 200;
     staticFile(res, publicUrl, extname);
